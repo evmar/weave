d470be6a3ec4120f96372fe998ee03ccc1b8e208
Evan Martin <evan.martin@gmail.com>
2022-04-23 16:37:53 -0700

    more split

---
+++ wasm/sections.ts
@@ -0,0 +1,205 @@
+import * as code from './code';
+import { Reader } from './reader';
+import {
+  DescGlobal,
+  DescIndex,
+  DescMem,
+  DescTable,
+  descToString,
+  DescType,
+  GlobalType,
+  readGlobalType,
+  readLimits,
+  readTable,
+  TableType,
+} from './shared';
+import { FuncType, readFuncType } from './type';
+
+export enum SectionType {
+  custom = 'custom',
+  type = 'type',
+  import = 'import',
+  function = 'function',
+  table = 'table',
+  memory = 'memory',
+  global = 'global',
+  export = 'export',
+  start = 'start',
+  element = 'element',
+  code = 'code',
+  data = 'data',
+  data_count = 'data count',
+}
+
+export interface CustomSection {
+  name: string;
+}
+
+/** Leaves the reader in a state ready to read the content of the section. */
+export function readCustomSection(r: Reader): CustomSection {
+  const name = r.name();
+  return { name };
+}
+
+export interface NameSection {
+  moduleName?: string;
+  functionNames?: Map<number, string>;
+  globalNames?: Map<number, string>;
+  dataNames?: Map<number, string>;
+}
+
+function readNameMap(r: Reader): Map<number, string> {
+  const len = r.readUint();
+  const map = new Map();
+  for (let i = 0; i < len; i++) {
+    map.set(r.readUint(), r.name());
+  }
+  return map;
+}
+
+export function readNameSection(r: Reader): NameSection {
+  let sec: NameSection = {};
+  while (!r.done()) {
+    const b = r.read8();
+    const size = r.readUint();
+    // https://github.com/WebAssembly/extended-name-section/blob/main/proposals/extended-name-section/Overview.md
+    switch (b) {
+      case 0:
+        sec.moduleName = r.name();
+        break;
+      case 1:
+        sec.functionNames = readNameMap(r);
+        break;
+      case 2:
+        console.warn(`unimplemented 'local name' subsection`);
+        r.skip(size);
+        break;
+      case 7:
+        sec.globalNames = readNameMap(r);
+        break;
+      case 9:
+        sec.dataNames = readNameMap(r);
+        break;
+      default:
+        console.warn(`ignoring unknown name subsection id ${b.toString(16)}`);
+        r.skip(size);
+        break;
+    }
+  }
+  return sec;
+}
+
+export function readTypeSection(r: Reader): FuncType[] {
+  return r.vec(readFuncType);
+}
+
+export interface Import {
+  module: string;
+  name: string;
+  desc: DescIndex<DescType.typeidx> | DescTable | DescMem | DescGlobal;
+}
+
+export function importToString(imp: Import): string {
+  return `${imp.module}.${imp.name} (${descToString(imp.desc)})`;
+}
+
+export function readImportSection(r: Reader): Import[] {
+  return r.vec(() => {
+    const module = r.name();
+    const name = r.name();
+    const desc8 = r.read8();
+    let desc: Import['desc'];
+    switch (desc8) {
+      case 0:
+        desc = { type: DescType.typeidx, index: r.readUint() };
+        break;
+      case 1:
+        desc = { type: DescType.table, table: readTable(r) };
+        break;
+      case 2:
+        desc = { type: DescType.mem, limits: readLimits(r) };
+        break;
+      case 3:
+        desc = { type: DescType.global, globalType: readGlobalType(r) };
+        break;
+      default:
+        throw new Error(`unhandled import desc type ${desc8.toString(16)}`);
+    }
+    return { module, name, desc };
+  });
+}
+
+export function readFunctionSection(r: Reader): number[] {
+  return r.vec(() => r.readUint());
+}
+
+export function readTableSection(r: Reader): TableType[] {
+  return r.vec(() => readTable(r));
+}
+
+export interface Global {
+  type: GlobalType;
+  init: code.Instruction[];
+}
+export function readGlobalSection(r: Reader): Global[] {
+  return r.vec(() => {
+    return { type: readGlobalType(r), init: code.readExpr(r) };
+  });
+}
+
+export interface Export {
+  name: string;
+  desc:
+    | DescIndex<DescType.funcidx>
+    | DescIndex<DescType.tableidx>
+    | DescIndex<DescType.memidx>
+    | DescIndex<DescType.globalidx>;
+}
+export function exportToString(exp: Export): string {
+  return `${exp.name} (${descToString(exp.desc)})`;
+}
+export function readExportSection(r: Reader): Export[] {
+  return r.vec(() => {
+    const name = r.name();
+    const desc8 = r.read8();
+    let desc: Export['desc'];
+    const type = (
+      [
+        DescType.funcidx,
+        DescType.tableidx,
+        DescType.memidx,
+        DescType.globalidx,
+      ] as const
+    )[desc8];
+    if (!type) {
+      throw new Error(`unhandled export desc type ${desc8.toString(16)}`);
+    }
+    return { name, desc: { type, index: r.readUint() } };
+  });
+}
+
+export interface DataSectionData {
+  init: DataView;
+  memidx?: number;
+  offset?: code.Instruction[];
+}
+export function readDataSection(r: Reader): DataSectionData[] {
+  return r.vec(() => {
+    const flags = r.read8();
+    switch (flags) {
+      case 0: {
+        const expr = code.readExpr(r);
+        const len = r.readUint();
+        return { init: r.slice(len), memidx: 0, offset: expr };
+      }
+      case 1: {
+        const len = r.readUint();
+        return { init: r.slice(len) };
+      }
+      default:
+        throw new Error(
+          `unhandled data section data flags ${flags.toString(16)}`
+        );
+    }
+  });
+}
---
+++ wasm/shared.ts
@@ -0,0 +1,117 @@
+/** Common Wasm types found across sections. */
+
+import { Reader } from './reader';
+import { readValType, Type } from './type';
+
+export enum DescType {
+  table = 'table',
+  mem = 'mem',
+  global = 'global',
+
+  funcidx = 'funcidx',
+  typeidx = 'typeidx',
+  tableidx = 'tableidx',
+  memidx = 'tableidx',
+  globalidx = 'globalidx',
+}
+
+export interface DescTable {
+  type: DescType.table;
+  table: TableType;
+}
+
+export interface DescMem {
+  type: DescType.mem;
+  limits: Limits;
+}
+
+export interface DescGlobal {
+  type: DescType.global;
+  globalType: GlobalType;
+}
+
+export interface DescIndex<T> {
+  type: T;
+  index: number;
+}
+type DescIndexTypes =
+  | DescIndex<DescType.funcidx>
+  | DescIndex<DescType.typeidx>
+  | DescIndex<DescType.tableidx>
+  | DescIndex<DescType.memidx>
+  | DescIndex<DescType.globalidx>;
+
+export function descToString(
+  desc: DescTable | DescMem | DescGlobal | DescIndexTypes
+): string {
+  switch (desc.type) {
+    case DescType.table:
+      return `table ${desc.table}`;
+    case DescType.mem:
+      return `mem ${limitsToString(desc.limits)}`;
+    case DescType.global:
+      return `mem ${desc}`;
+    default:
+      return `${desc.type} index ${desc.index}`;
+  }
+}
+
+export interface Limits {
+  minimum: number;
+  maximum?: number;
+}
+
+export function readLimits(r: Reader): Limits {
+  const b = r.read8();
+  let minimum: number;
+  let maximum: number | undefined;
+  switch (b) {
+    case 0:
+      minimum = r.readUint();
+      break;
+    case 1:
+      minimum = r.readUint();
+      maximum = r.readUint();
+      break;
+    default:
+      throw new Error(`invalid limits flag ${b.toString(16)}`);
+  }
+  return { minimum, maximum };
+}
+
+export function limitsToString(limits: Limits) {
+  return `min=${limits.minimum} max=${limits.maximum ?? 'none'}`;
+}
+
+export interface TableType {
+  element: Type;
+  limits: Limits;
+}
+
+export function readTable(r: Reader): TableType {
+  const element = readValType(r);
+  const limits = readLimits(r);
+  return { element, limits };
+}
+
+export interface GlobalType {
+  valType: Type;
+  mut: boolean;
+}
+
+export function readGlobalType(r: Reader): GlobalType {
+  const valType = readValType(r);
+  const mutB = r.read8();
+  let mut;
+  switch (mutB) {
+    case 0:
+      mut = false;
+      break;
+    case 1:
+      mut = true;
+      break;
+    default:
+      throw new Error(`invalid mutability flag ${mutB.toString(16)}`);
+  }
+  return { valType, mut };
+}
