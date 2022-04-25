e42feb178f63f13da44ad5b86d815ac32340296f
Evan Martin <evan.martin@gmail.com>
2022-04-25 15:37:12 -0700

    remove hooks from data

--- viz/data.tsx
+++ viz/data.tsx
@@ -1,7 +1,7 @@
 import * as wasm from 'wasm';
 import { Indexed, ParsedModule, Screen } from './viz';
 import { h } from 'preact';
-import * as hooks from 'preact/hooks';
+import * as preact from 'preact';
 import { Instructions } from './code';
 import { Column, Table } from './table';
 import * as d3 from 'd3';
@@ -47,52 +47,63 @@ function hex(byte: number, pad = 2): string {
   return byte.toString(16).padStart(pad, '0');
 }
 
+namespace HexView {
+  export interface Props {
+    data: DataView;
+  }
+  export interface State {
+    hover?: number;
+  }
+}
+export class HexView extends preact.Component<HexView.Props, HexView.State> {
+  render() {
+    const visibleRows = 20;
+    const view = this.props.data;
+    const rows = [];
+    for (let row = 0; row < visibleRows && row * 16 < view.byteLength; row++) {
+      const hexBytes = [];
+      const vizBytes = [];
+      for (let col = 0; col < 16; col++) {
+        const index = row * 16 + col;
+        if (index >= view.byteLength) break;
+        const byte = view.getUint8(index);
+        const hexByte = hex(byte);
+        const vizByte =
+          byte >= 0x20 && byte < 0x7f ? String.fromCharCode(byte) : '.';
+        hexBytes.push(
+          ' ',
+          <span
+            onMouseOver={() => this.setState({ hover: index })}
+            className={index === this.state.hover ? 'highlight' : ''}
+          >
+            {hexByte}
+          </span>
+        );
+        vizBytes.push(
+          <span
+            onMouseOver={() => this.setState({ hover: index })}
+            className={index === this.state.hover ? 'highlight' : ''}
+          >
+            {vizByte}
+          </span>
+        );
+      }
+      rows.push(
+        <div>
+          {hex(row * 16, 6)} {hexBytes}
+          {'  '}
+          {vizBytes}
+        </div>
+      );
+    }
+    return <pre>{rows}</pre>;
+  }
+}
+
 export function DataHex(props: {
   module: ParsedModule;
   data: Indexed<wasm.DataSectionData>;
 }) {
-  const [hover, setHover] = hooks.useState<number | null>(null);
-
-  const visibleRows = 20;
-  const view = props.data.init;
-  const rows = [];
-  for (let row = 0; row < visibleRows && row * 16 < view.byteLength; row++) {
-    const hexBytes = [];
-    const vizBytes = [];
-    for (let col = 0; col < 16; col++) {
-      const index = row * 16 + col;
-      if (index >= view.byteLength) break;
-      const byte = view.getUint8(index);
-      const hexByte = hex(byte);
-      const vizByte =
-        byte >= 0x20 && byte < 0x7f ? String.fromCharCode(byte) : '.';
-      hexBytes.push(
-        ' ',
-        <span
-          onMouseOver={() => setHover(index)}
-          className={index === hover ? 'highlight' : ''}
-        >
-          {hexByte}
-        </span>
-      );
-      vizBytes.push(
-        <span
-          onMouseOver={() => setHover(index)}
-          className={index === hover ? 'highlight' : ''}
-        >
-          {vizByte}
-        </span>
-      );
-    }
-    rows.push(
-      <div>
-        {hex(row * 16, 6)} {hexBytes}
-        {'  '}
-        {vizBytes}
-      </div>
-    );
-  }
-
   return (
     <Screen module={props.module} title={`data[${props.data.index}]`}>
       <div>
@@ -102,7 +113,7 @@ export function DataHex(props: {
         <b>init</b>:{' '}
         <Instructions module={props.module} instrs={props.data.offset!} />
       </div>
-      <pre>{rows}</pre>
+      <HexView data={props.data.init} />
     </Screen>
   );
 }
